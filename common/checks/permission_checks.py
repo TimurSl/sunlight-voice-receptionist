036caae5d32a0a0fe68b0e32d9ab54224c37810c
Tymur Slynko <timurslinko1@gmail.com>
2025-04-27 13:08:14 +0300

    Fixed errors

--- common/checks/permission_checks.py
+++ common/checks/permission_checks.py
@@ -12,15 +12,6 @@ def is_moderator():
         return MODERATOR_ROLE_ID in [r.id for r in ctx.author.roles]
     return commands.check(predicate)
 
-AI_USER_ROLE_ID = int(os.getenv("DISCORD_AI_USER_ROLE_ID"))
-
-def is_ai_user():
-    async def predicate(ctx):
-        if ctx.author.guild_permissions.administrator:
-            return True
-        return AI_USER_ROLE_ID in [r.id for r in ctx.author.roles]
-    return commands.check(predicate)
-
 MUSIC_USER_ROLE_ID = int(os.getenv("DISCORD_MUSIC_ROLE_ID"))
 
 def is_music_user():
--- main.py
+++ main.py
@@ -3,14 +3,7 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 
-from cogs.Notifier import Notifier
-from commands.useful.chat_cleaner import ChatCleaner
-from commands.fun.random_picture import RandomPicture
-from commands.ai.ask_a_bot import AskAI
 from cogs.voice_rooms import VoiceRooms
-from cogs.stats import ServerStats
-
-
 
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
@@ -24,13 +17,7 @@ intents.message_content = True
 
 class CustomBot(commands.Bot):
     async def setup_hook(self):
-
-        await bot.add_cog(Notifier(bot))
-        await bot.add_cog(ChatCleaner(bot))
-        await bot.add_cog(RandomPicture(bot))
-        await bot.add_cog(AskAI(bot))
         await bot.add_cog(VoiceRooms(bot))
-        await bot.add_cog(ServerStats(bot))
 
 
         print("Reloaded all modules & synced commands.")
